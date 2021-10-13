c40eca699e6549de660ca43805a8ff01121256bd
Arynn Boniface <arynn.boniface@gmail.com>
2021-10-13 13:54:02 -0400

    outlining testmonial component

--- src/components/testimonials/Testimonial.js
+++ src/components/testimonials/Testimonial.js
@@ -3,10 +3,13 @@ import React from 'react';
 // STYLES
 import styles from '../../assets/style/testimonials/testimonial.module.scss';
 
-const Testimonial = ({image, name, }) => {
+const Testimonial = ({ avatar, text, name, role }) => {
   return (
     <section>
-      <img src={} />
+      <img src={avatar} alt={`${name}'s profile avatar.`} />
+      <p>{text}</p>
+      <h1>{name}</h1>
+      <p className={styles.role}>{role}</p>
     </section>
   );
 }; 
